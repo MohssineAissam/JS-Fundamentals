2b41709cfa5eecee7bbe20724cadf2a16d9247c7
Aissam <koala3901@gmail.com>
2025-05-24 22:51:28 +0100

    SS

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,9 @@
-const arg = process.argv[2];
-const count = parseInt(arg, 10);
+const count = Number(process.argv[2]);
 
-if (!arg || isNaN(count) || count <= 0) {
-  console.log("Missing number of occurrences");
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
 } else {
-  let i = 0;
-  while (i < count) {
-    console.log("C is fun");
-    i++;
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
   }
 }
